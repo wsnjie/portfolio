cac43eaa4cbccec7501ad551c9eae4fe5e484c18
Will <megapocketninja@gmail.com>
2018-12-31 17:05:09 -0500

    Added instagram feed to about me page

---
+++ main.js
@@ -0,0 +1,11 @@
+let feed = new Instafeed({
+    target: 'instafeed',
+    get: 'user',
+    userId: '217204082',
+    clientId: '68e09ca7d86e47edbb5c140640ea3fda',
+    accessToken: '217204082.1677ed0.637cf7479ac845ab937d1aef2dea3849',
+    sortBy: 'random',
+    resolution: 'low_resolution',
+    limit: 9
+})
+feed.run()
